d44d4194c246a6ff75f191202e39c3d0b1865b55
Jimena <0281413@up.edu.mx>
2026-05-04 23:40:30 -0600

    Initial Commit

---
+++ Nubi/NubiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NubiApp.swift
+//  Nubi
+//
+//  Created by Jimena Rodriguez on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct NubiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
